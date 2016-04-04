8340664b1454bb77a03328766caebaed205fb559
nimayd <ntdesai2@illinios.edu>
2016-04-04 11:31:31 -0500

    sends all logs in cwd to visualizer

--- run_test.py
+++ run_test.py
@@ -1,20 +1,23 @@
 import socket
 import time
 import sys
+import os
 
 IP = "127.0.0.1"
 PORT = 1234
 
-def main(output_file):
+def main():
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.sendto("START,666", (IP, PORT))
-    with open(output_file, "r") as f:
-        for line in f:
-            sock.sendto(line, (IP, PORT))
+    for fl in os.listdir(os.getcwd()):
+        if fl.endswith(".txt"):
+            with open(fl, "r") as f:
+                for line in f:
+                    sock.sendto(line, (IP, PORT))
 
 
 """
 For testing the visualaztion tool.
 """
 if __name__ == '__main__':
-    main(sys.argv[1])
+    main()
